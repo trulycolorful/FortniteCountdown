654510d7844aafbe7f641dda0d703d2addbb323b
trulycolorful <39491418+trulycolorful@users.noreply.github.com>
2018-11-22 23:42:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,18 +4,24 @@ const client = new Discord.Client();
 var counting = false;
 
 client.on('message', message => {
-  if (message.content === "!count" && !counting && message.member.roles.find('name', 'Queue Snipes Organizer')) {
-    counting = true;
-    var channel = client.channels.find('name', 'Countdown');
-    channel.join()
-      .then(connection => {
-        const dispatcher = connection.playFile('./countdown.wav');
-        dispatcher.on('end', end => { 
-          channel.leave();
-          counting = false;
-        });
-      })
-      .catch(err => console.log(err));
+  let role = message.guild.roles.find(role => role.name === "Muted");
+  let darrin = message.guild.members.get('502226394789445632');
+  let bear = message.guild.members.get('512462639377678368');;
+  
+  if (message.content === "!mutebear") {
+    bear.addRole(role);
+  }
+  
+  if (message.content === "!mutedarrin") {
+    darrin.addRole(role);
+  }
+  
+  if (message.content === "!unmutebear") {
+    bear.removeRole(role);
+  }
+  
+  if (message.content === "!unmutedarrin") {
+    darrin.removeRole(role);
   }
 });
 
